7f4905db98481a2696821c42708f85c14a52a99a
KausaChangs <changayakausa@gmail.com>
2023-01-02 10:14:08 +0200

    added notification object and console log eventListener

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   Notification.requestPermission().then((perm) => {
     if (perm === "granted") {
-      new Notification("Example Notification", {
+      const notification = new Notification("Example Notification", {
         body: "this is more text",
+        data: "world",
+      });
+
+      notification.addEventListener("close", (e) => {
+        console.log(e);
       });
     }
   });
