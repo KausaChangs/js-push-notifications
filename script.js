9558440e30ad202fe7f709a193ffe49db50fcf6e
KausaChangs <changayakausa@gmail.com>
2023-01-02 10:07:50 +0200

    added a body object to Notification

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const button = document.querySelector("button");
 button.addEventListener("click", () => {
   Notification.requestPermission().then((perm) => {
     if (perm === "granted") {
-      new Notification("Example Notification");
+      new Notification("Example Notification", {
+        body: "this is more text",
+      });
     }
   });
 });
